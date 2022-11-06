28829eb8175c28e541e6f74a260bdb021a1026c2
van133 <vkn123@naver.com>
2022-11-06 14:19:59 +0900

    2

--- spring_api/src/main/java/springrestapi/demo/entity/Order.java
+++ spring_api/src/main/java/springrestapi/demo/entity/Order.java
@@ -1,28 +1,26 @@
 package springrestapi.demo.entity;
-
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.*;
-
-public class User {
-
+public class Order {
     @Id // 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY) //
+    private long order_id;
+
+    @Column(nullable = false) // not null
+    private long shop_id;
+
+    @Column(nullable = false) // not null
+    private long menu_id;
+
+    @Column(length = 10, nullable = false) // 10글자 제한, not null
     private long user_id;
 
     @Column(nullable = false) // not null
-    private String sign_id;
+    private String address;
 
     @Column(nullable = false) // not null
-    private String user_password;
+    private long price;
 
-    @Column(length = 10, nullable = false) // 10글자 제한, not null
-    private String name;
-
-    @Column(nullable = false) // not null
-    private Integer poing;
-
-    @Column(nullable = false) // not null
-    private long order_id;
 }
